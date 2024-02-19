9f2ead4c3eb904d171b2d8a06378ab1f0d655255
Tadas <tadas.gat@gmail.com>
2024-02-19 19:28:10 +0200

    Klasės skirtos Programos-DB-Programos bendravimui.

---
+++ MitybosPrograma/Assets/Scripts/ClientMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public class ClientMethods : IClientInterface
+{
+	public ClientMethods()
+	{
+		private readonly DatabaseMethods _databaseMethods;
+
+		public ClientMethods(DatabaseMethods databaseMethods)
+		{
+		this._databaseMethods = databaseMethods;
+		}
+		
+
+	}
+}
---
+++ MitybosPrograma/Assets/Scripts/DatabaseMethods.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class DatabaseMethods
+{
+	public DatabaseMethods()
+	{
+	}
+}
