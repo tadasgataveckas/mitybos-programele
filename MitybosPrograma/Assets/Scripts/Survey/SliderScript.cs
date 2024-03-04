0407a2a1c1db7011c64cca7cc23da2f02ed9be82
MatasPal <102602807+MatasPal@users.noreply.github.com>
2024-03-04 20:57:25 +0200

    Added BMI and daily calories calculating methods, added slider descriptions

--- MitybosPrograma/Assets/Scripts/Survey/SliderScript.cs
+++ MitybosPrograma/Assets/Scripts/Survey/SliderScript.cs
@@ -24,23 +24,23 @@ public class SliderScript : MonoBehaviour
         sliderText.text = slider.value.ToString();
         if (slider.value == 1)
         {
-            descriptionText.text = "Low";
+            descriptionText.text = "Group I - those in very light manual work (students, civil servants, people with mental jobs). " +
+                "This group has a FAL of 1.2 - very low activity (sedentary or no physical activity).";
         }
         else if (slider.value == 2)
         {
-            descriptionText.text = "Medium";
+            descriptionText.text = "Group II - light manual workers (doctors, nurses, agronomists, drivers, service workers, etc.). " +
+                "FAL is 1.375 - low activity 1 to 3 times a week.";
         }
         else if (slider.value == 3)
         {
-            descriptionText.text = "High";
+            descriptionText.text = "Group III - those engaged in moderate physical work (surgeons, light industrial workers, managers, " +
+                "housewives, utility workers and people engaged in mechanised manual work). FAL is 1.55 - average activity 3 to 5 times a week.";
         }
         else if (slider.value == 4)
         {
-            descriptionText.text = "Rare";
-        }
-        else if (slider.value == 5)
-        {
-            descriptionText.text = "Super";
+            descriptionText.text = "Group IV: those engaged in heavy manual work (athletes, farmers, construction workers and workers in " +
+                "other industries engaged in non-mechanised and semi-mechanised work). FAL is 1.725 - high activity 6 to 7 times a week.";
         }
     }
 }
--- MitybosPrograma/Assets/Scripts/Survey/SurveyManager.cs
+++ MitybosPrograma/Assets/Scripts/Survey/SurveyManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -23,6 +24,8 @@ public class SurveyManager : MonoBehaviour
     public List<string> allergies;
 
     public int activity;
+
+    private double BMI;
 
 
     string constring = "Server=localhost;User ID=root;Password=root;Database=food_db";
@@ -97,6 +100,95 @@ public class SurveyManager : MonoBehaviour
         }
     }
 
+
+
+    //Calculating features
+
+    //Calculating BMI
+    public double CalculateBMI(double Bheight, double Bweight)
+    {
+        BMI = Bweight / Math.Pow(Bheight,2);
+        return BMI;
+    }
+
+    //Printing BMI result
+    public string BMIResult(double RBMI)
+    {
+        if(RBMI > 18.5)
+        {
+            return "Underweight";
+        }
+        else if (18.5 < RBMI && RBMI < 25.0)
+        {
+            return "Normal weight";
+        }
+        else if (25.0 < RBMI && RBMI < 30.0)
+        {
+            return "Overweight";
+        }
+        else if (RBMI > 30.0)
+        {
+            return "Obese";
+        }
+        return "";
+    }
+
+    //Calculating daily calories
+    public double CalculateDailyCalories(int activityLevel, string cGender, int cAge, double cWeight)
+    {
+        //daily calories
+        double dCalories = 0.0;
+
+        //basic metabolism
+        double PMA = 0.0;
+
+        //physical activity level
+        double FAL = 0.0;
+
+        if (activityLevel == 1)
+        {
+            FAL = 1.2;
+        }
+        else if (activityLevel == 2)
+        {
+            FAL = 1.375;
+        }
+        else if (activityLevel == 3)
+        {
+            FAL = 1.55;
+        }
+        else if (activityLevel == 4)
+        {
+            FAL = 1.725;
+        }
+
+        if(cGender == "Woman")
+        {
+            if (cAge < 30)
+            {
+                PMA = (0.0615 * cWeight + 2.08) * 240;
+            }
+            else if(cAge < 60)
+            {
+                PMA = (0.0364 * cWeight + 3.47) * 240;
+            }
+        }
+        else
+        {
+            if (cAge < 30)
+            {
+                PMA = (0.064 * cWeight + 2.84) * 240;
+            }
+            else if (cAge < 60)
+            {
+                PMA = (0.0485 * cWeight + 3.67) * 240;
+            }
+        }
+
+        //daily calories = metabolism * physical activity level
+        dCalories = PMA * FAL;
+        return dCalories;
+    }
 
 
 
