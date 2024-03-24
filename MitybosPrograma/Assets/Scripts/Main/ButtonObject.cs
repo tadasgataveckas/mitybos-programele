c7eb643f38ac020e776925bc1a2ee51940ed14ae
Ignaseismantas <45782450+Ignaseismantas@users.noreply.github.com>
2024-03-24 13:04:20 +0200

    Deleted Soft Buttons, added mini game title cards, started adding input system

---
+++ MitybosPrograma/Assets/Scripts/Main/ButtonObject.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+using UnityEngine.EventSystems;
+using TMPro;
+using System.Collections;
+
+public class ButtonObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Enter");
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exit");
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("DebugName");
+
+        if (eventData.button == PointerEventData.InputButton.Left) // LMB
+        {
+        }
+        if (eventData.button == PointerEventData.InputButton.Right) // RMB
+        {
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left) // LMB
+        {
+          
+        }
+    }
+    public void DebugLog()
+    {
+        Debug.Log("DebugName");
+    }
+}
