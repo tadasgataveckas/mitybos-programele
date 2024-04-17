e0898bffab22e9001927ba5e7988dcbd4febed0e
Tadas Gataveckas <115645380+tadasgataveckas@users.noreply.github.com>
2024-04-17 22:01:30 +0300

    Atvaizduojama data
    Jei nepasirinkta kalendoriuje - šiandienos, kitaip - pasirinkta data

--- MitybosPrograma/Assets/Scripts/PlanCreation/PlanCreation/AutoPlan.cs
+++ MitybosPrograma/Assets/Scripts/PlanCreation/PlanCreation/AutoPlan.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using TMPro;
 using UnityEditor;
+using UI.Dates;
 
 public class AutoPlan : MonoBehaviour
 {
@@ -12,14 +13,18 @@ public class AutoPlan : MonoBehaviour
     List<FoodClass> createdlist = new List<FoodClass>(); //meals for the day
     private void Awake()
     {
-        TMP_Text textobjectBreakfast = GameObject.Find("BreakfastFoodnameText").GetComponent<TMP_Text>();
-        TMP_Text textobjectLunch = GameObject.Find("LunchFoodnameText").GetComponent<TMP_Text>();
-        TMP_Text textobjectDinner = GameObject.Find("DinnerFoodnameText").GetComponent<TMP_Text>();
-        
+        //TMP_Text textobjectBreakfast = GameObject.Find("BreakfastFoodnameText").GetComponent<TMP_Text>();
+        //TMP_Text textobjectLunch = GameObject.Find("LunchFoodnameText").GetComponent<TMP_Text>();
+        //TMP_Text textobjectDinner = GameObject.Find("DinnerFoodnameText").GetComponent<TMP_Text>();
+        //TMP_Text textobjectDate = GameObject.Find("SelectedDateText").GetComponent<TMP_Text>();
 
     }
 
-
+    private void Update()
+    {
+        
+        OnSelectedDateUpdate();
+    }
     public void OnClickAutoPlan()
     {
         returnedfoodlist = c.ReturnFoodList();
@@ -29,8 +34,19 @@ public class AutoPlan : MonoBehaviour
         TMP_Text textobjectBreakfast = GameObject.Find("BreakfastFoodnameText").GetComponent<TMP_Text>();
         TMP_Text textobjectLunch = GameObject.Find("LunchFoodnameText").GetComponent<TMP_Text>();
         TMP_Text textobjectDinner = GameObject.Find("DinnerFoodnameText").GetComponent<TMP_Text>();
-        textobjectBreakfast.text = createdlist[0].ToString();
-        textobjectLunch.text = createdlist[1].ToString();
-        textobjectDinner.text = createdlist[2].ToString();
+        textobjectBreakfast.text = createdlist[0]?.ToString();
+        textobjectLunch.text = createdlist[1]?.ToString();
+        textobjectDinner.text = createdlist[2]?.ToString();
+        
+    }
+
+    public void OnSelectedDateUpdate()
+    {
+        TMP_Text textobjectDate = GameObject.Find("SelectedDateText").GetComponent<TMP_Text>();
+        DatePicker datePicker = GameObject.Find("DatePickerParent").GetComponent<DatePicker>();
+        if (datePicker != null)
+        {
+            textobjectDate.text = "Plan for: " + datePicker.VisibleDate.ToString().Truncate(10,"");
+        }
     }
 }
