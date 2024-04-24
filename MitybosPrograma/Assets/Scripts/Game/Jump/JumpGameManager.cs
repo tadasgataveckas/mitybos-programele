762909e13136eb5fcd362ab716a33bd0183b4c52
ErnestasKu <127497707+ErnestasKu@users.noreply.github.com>
2024-04-25 00:50:20 +0300

    Added high score system
    - high scores will be stored in db
    - also added reusable high score leaderboard prefab

--- MitybosPrograma/Assets/Scripts/Game/Jump/JumpGameManager.cs
+++ MitybosPrograma/Assets/Scripts/Game/Jump/JumpGameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class JumpGameManager : MonoBehaviour
 {
@@ -23,6 +24,7 @@ public class JumpGameManager : MonoBehaviour
     public Transform cameraTransform;
 
     public int spawnRelaxStationsPerXft = 100;
+    [SerializeField] private Scoreboard scoreboard;
 
     void Awake()
     {
@@ -44,24 +46,24 @@ public class JumpGameManager : MonoBehaviour
 
         for (int i = 0; i < platformCount; i++)
         {
-            spawnPosition.y += Random.Range(.2f, 1f);
-            spawnPosition.x = Random.Range(0.2f-screenWidthInUnits/2, screenWidthInUnits / 2 - 0.2f);
+            spawnPosition.y += UnityEngine.Random.Range(.2f, 1f);
+            spawnPosition.x = UnityEngine.Random.Range(0.2f - screenWidthInUnits / 2, screenWidthInUnits / 2 - 0.2f);
             // Relax Station
             if ((int)spawnPosition.y % spawnRelaxStationsPerXft == 0 && (int)spawnPosition.y != 0)
             {
                 Instantiate(relaxPlatformPrefab, new Vector3(0f, spawnPosition.y, 0f), Quaternion.identity);
                 spawnPosition.y += 2f;
-            } 
+            }
             else // Regular
             {
-                float percentage = Random.Range(0f, 100f);
+                float percentage = UnityEngine.Random.Range(0f, 100f);
                 // One time platform, 30%
                 if (percentage < 30)
                 {
                     Instantiate(oneTimePlatformPrefab, spawnPosition, Quaternion.identity);
                 }
                 // High jump platform, 10%
-                else if (percentage < 40) 
+                else if (percentage < 40)
                 {
                     Instantiate(highJumpPlatformPrefab, spawnPosition, Quaternion.identity);
                 }
@@ -70,31 +72,28 @@ public class JumpGameManager : MonoBehaviour
                 {
                     Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
                 }
-                
+
             }
         }
     }
 
     void Update()
     {
+        // Update max score
         if (player.position.y > maxScore)
         {
             maxScore = player.position.y;
             score.text = maxScore.ToString("F1") + " ft";
         }
+
         // Check if player is below the camera vertically
         if (player.position.y < cameraTransform.position.y - screenHeightInUnits / 2)
         {
-            // Game over
-            gameOver.SetActive(true);
-            player.gameObject.GetComponent<BoxCollider2D>().enabled = false;
-
-            // Restart scene after 2 seconds
-            Invoke("RestartScene", 2f);
+            TriggerGameOver();
         }
 
         // Check if player moves outside camera horizontally
-        if (Mathf.Abs(player.position.x - cameraTransform.position.x)-0.1f > screenWidthInUnits / 2)
+        if (Mathf.Abs(player.position.x - cameraTransform.position.x) - 0.1f > screenWidthInUnits / 2)
         {
             // Teleport the player to the other side of the camera horizontally
             Vector3 playerPos = player.position;
@@ -128,9 +127,75 @@ public class JumpGameManager : MonoBehaviour
         // Log the screen width in units
         //Debug.Log("Screen width in units: " + screenWidthInUnits);
     }
-    void RestartScene()
+    public void RestartScene()
     {
         // Restart the scene
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    private void TriggerGameOver()
+    {
+        // Game over
+        gameOver.SetActive(true);
+        player.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        PopulateScoreboard();
+
+        // added so Update() won't run while game is over
+        this.enabled = false;
+    }
+
+    // inserts score and populates scoreboard
+    private void PopulateScoreboard()
+    {
+        int id_user = SessionManager.GetIdKey();
+        if (id_user < 0)
+            return;
+
+        ScoresChickenWings.SaveScore(id_user, maxScore);
+        List<ScoresChickenWings> list = ScoresChickenWings.GetScoresById(id_user);
+
+        // finds and highlights current score index
+        int currentScoreIndex = -1;
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (System.Math.Round(list[i].score) == System.Math.Round(maxScore))
+            {
+                // if within top 10
+                if (i < 10)
+                {
+                    scoreboard.HighlightElement(i);
+                    break;
+                }
+                // if lower position than 10
+                else
+                {
+                    scoreboard.HighlightElement(9);
+                    currentScoreIndex = i;
+                    break;
+                }
+            }
+        }
+
+        // add 10 scores, if current is not in the top 10, show in place of 10th
+        for (int i = 0; i <= 9; i++)
+        {
+            // in case list is shorter than 10
+            if (i >= list.Count)
+                break;
+
+            // if score doesn't reach top 10
+            if (i == 9 && currentScoreIndex > 9)
+            {
+                scoreboard.AddScoreItem((currentScoreIndex + 1).ToString());
+                scoreboard.AddScoreItem(Math.Round(list[currentScoreIndex].score, 1).ToString() + " ft").alignment = TextAlignmentOptions.Left;
+                scoreboard.AddScoreItem(list[i].score_date.Substring(0, list[currentScoreIndex].score_date.Length - 9));
+            }
+            else
+            {
+                scoreboard.AddScoreItem((i + 1).ToString());
+                scoreboard.AddScoreItem(Math.Round(list[i].score, 1).ToString() + " ft").alignment = TextAlignmentOptions.Left;
+                scoreboard.AddScoreItem(list[i].score_date.Substring(0, list[i].score_date.Length - 9));
+            }
+        }
     }
 }
