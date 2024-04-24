46a469ddfeaf790991fa9a448725af01f230d463
MatasPal <102602807+MatasPal@users.noreply.github.com>
2024-04-24 12:28:24 +0300

    scrollers updated, height working

--- MitybosPrograma/Assets/Scripts/Survey/PhysicalScrollers.cs
+++ MitybosPrograma/Assets/Scripts/Survey/PhysicalScrollers.cs
@@ -1,45 +1,41 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class PhysicalScrollers : MonoBehaviour
 {
     public ScrollRect scrollRect;
     public GameObject[] objects; // Čia įrašykite objektus, kuriuos norite scroll'inti
     private RectTransform contentRect;
+    public Transform numberParent;
+    public GameObject numberPrefab;
+
 
     void Start()
     {
-        // Paimame ScrollView turinio RectTransform
-        contentRect = scrollRect.content.GetComponent<RectTransform>();
-
-        // Prijungiame OnValueChanged įvykį prie mūsų metodo
-        scrollRect.onValueChanged.AddListener(OnScrollValueChanged);
+        
+        AddNumbers();
     }
 
     void Update()
     {
-        scrollRect.onValueChanged.AddListener(OnScrollValueChanged);
+        
+        //GetValue();
     }
 
-    // Metodas, kuris bus iškviestas kai keičiasi ScrollView reikšmė
-    public void OnScrollValueChanged(Vector2 scrollPos)
+    
+    public void AddNumbers()
     {
-        // Gauti vertikalių poziciją, kurioje yra ScrollRect centravimas
-        float centerY = scrollPos.y * (contentRect.rect.height - scrollRect.viewport.rect.height);
-
-        // Einame per visus objektus ir tikriname, ar objektas yra centriniame regione
-        foreach (GameObject obj in objects)
-        {
-            RectTransform objRect = obj.GetComponent<RectTransform>();
-            float objY = objRect.localPosition.y;
-
-            // Tikriname, ar objektas yra centriniame regione
-            if (objY <= centerY && objY + objRect.rect.height >= centerY)
-            {
-                Debug.Log("Selected object: " + obj.name);
-                // Darykite tai, ką norite su šiuo objektu
-                break; // Jei norite rasti tik vieną objektą, tai nutraukite ciklą
-            }
+        for (int i = 100; i <= 230; i++) {
+            GameObject spawnedNumber = Instantiate(numberPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity, numberParent);
+            spawnedNumber.GetComponent<TextMeshProUGUI>().text =  i.ToString();//'\n' +
         }
+    }
+
+    public void GetValue()
+    {
+        float value = (float)Math.Round(numberParent.localPosition.y / 100) + 100 ;
+        Debug.Log("Value: " + value);
     }
 }
